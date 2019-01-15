09cf6cef1cb57492b3b271ed48e6e90163016825
Armenio Maurício Ferreira Neto <dehme.armenio@gmail.com>
2019-01-15 16:07:01 -0200

    receive style background from get request

--- front/home.js
+++ front/home.js
@@ -1,15 +1,26 @@
 window.onload = function () {
+
     getCss();
+    console.log("dentro do onload")
 };
 
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("antes de carregar a page")
+}, false);
+
+
+
 function getCss() {
-    const axios = require('axios');
+
+    //const axios = require('axios');
 
     // Make a request for a user with a given ID
-    axios.get('/user?ID=12345')
+    axios.get('http://127.0.0.1:5000/css')
         .then(function (response) {
             // handle success
             console.log(response);
+            console.log("send response to change Css")
+            changeCss(response.data);
         })
         .catch(function (error) {
             // handle error
@@ -18,5 +29,14 @@ function getCss() {
         .then(function () {
             // always executed
         });
+    //console.log(response)
+}
 
+function changeCss(json) {
+
+    document.getElementById("idAzul").style.background = json.header;
+    document.getElementById("idVermelho").style.background = json.body;
+    document.getElementById("idAmarelo").style.background = json.footer;
+    console.log("Dentro do change css")
+    console.log(json)
 }
